55f8e93e6944c3cbe5850c0c47aec2738e8fbf0a
Mayur Mahajan <mayur23smahajan@gmail.com>
2021-12-03 23:11:11 +0530

    Grammar Correction

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,20 +13,13 @@ function AboutMe() {
                 </div>
                 <div className="profile-info">
                     <h2>Mayur S. Mahajan</h2>
-                    <p>Hi! I'm Mayur, a Developer, experienced in Web and App development. I am currently learning
-                    Data Science and Machine Learning. I'm an ardent believer of the learn-by-doing approach.</p>
+                    <p>Hi! I'm Mayur, a Developer, experienced in Web and App development. I am currently learning Data Science and Machine Learning. I'm an ardent believer in the learn-by-doing approach.</p>
 
-                    <p className="additional-about">I have done tons of software development projects over the years.
-                    I also worked as Full Stack Development Intern at Value Wealth Creators
-                    I have a youtube channel where I try to 
-                    solve frontend challenges like.</p>
+                    <p className="additional-about">I have done tons of software development projects over the years. I also worked as a Full Stack Development Intern at Value Wealth Creators I have a youtube channel where I try to solve front-end challenges.</p>
 
-                    <p className="additional-about">I spend most of my time learning new technologies and frameworks. I like to do competitive programming and solve algorithm-related problems on
-                    Codechef. When I am not coding, I like to design UI/UX on Figma.  </p>
+                    <p className="additional-about">I spend most of my time learning new technologies and frameworks. I like to do competitive programming and solve algorithm-related problems on CodeChef. When I am not coding, I like to design UI/UX on Figma. </p>
 
-                    <p className="additional-about">In my free-time I listen to music. I am a huge supporter of Manchester United FC and Mumbai Indians.
-                    I am a casual reader. But generally you will find me watching a movie or a series.
-                    </p>
+                    <p className="additional-about">In my free time, I listen to music. I am a huge supporter of Manchester United FC and Mumbai Indians. I am a casual reader. But generally, you will find me watching a movie or a series.        </p>
 
                 </div>
                 
