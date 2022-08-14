8d0c649e7ef710da178b31a03e8f7244a03a2b84
Mayur Mahajan <47064215+MayurSMahajan@users.noreply.github.com>
2022-08-14 22:05:50 +0530

    Updated About Me Section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -13,13 +13,13 @@ function AboutMe() {
                 </div>
                 <div className="profile-info">
                     <h2>Mayur S. Mahajan</h2>
-                    <p>Hi! I'm Mayur, a Developer, experienced in Web and App development. I am currently learning Data Science and Machine Learning. I'm an ardent believer in the learn-by-doing approach.</p>
+                    <p>Hi! I'm Mayur, a Developer, experienced in Web and App development. I am currently learning WEB3.0 development with ICP. I'm an ardent believer in the learn-by-doing approach.</p>
 
-                    <p className="additional-about">I have done tons of software development projects over the years. I also worked as a Full Stack Development Intern at Value Wealth Creators I have a youtube channel where I try to solve front-end challenges.</p>
+                    <p className="additional-about">I have done tons of software development projects over the years. I also worked as a Flutter Development Intern and Full Stack Development Intern. I have a youtube channel where I try to solve front-end challenges.</p>
 
-                    <p className="additional-about">I spend most of my time learning new technologies and frameworks. I like to do competitive programming and solve algorithm-related problems on CodeChef. When I am not coding, I like to design UI/UX on Figma. </p>
+                    <p className="additional-about">I spend most of my time learning new technologies and frameworks. I like to do open source contributions. I've contributed to Flutter WebRTC package, to React Docs and other interesting Open Source Projects. When I am not coding, I like to design UI/UX on Figma. </p>
 
-                    <p className="additional-about">In my free time, I listen to music. I am a huge supporter of Manchester United FC and Mumbai Indians. I am a casual reader. But generally, you will find me watching a movie or a series.        </p>
+                    <p className="additional-about">In my free time, I listen to music. I am a huge fan of Manchester United FC and Mumbai Indians. I am a casual reader, my favourite book is Atomic Habits by James Clear. But most often, you will find me watching a movie or a series.</p>
 
                 </div>
                 
