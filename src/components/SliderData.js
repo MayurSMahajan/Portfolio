7127b5133778cff890f9054c204f66d926bb5660
Mayur-Mahajan-Realtimate <mayur@realtimate.in>
2025-06-17 02:34:10 +0530

    feat: added the most recent complete project

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,57 +1,58 @@
 export const SliderData = [
   {
     image:
-    'images/sih.png',
-    title: 
-    'Daemon - Speeches Playing App',
+      'images/transformx-snapshot.png',
+    title:
+      'Transformx - Habit Tracking App Based on Atomic Habits',
     description:
-     'This is a cross platform app I created for Smart India Hackathon 2022. This app allows users to watch videos or listen to audios of speeches that are AI generated. The backend is linked with Firebase and is used to store these speeches.',
-     link:'https://github.com/MayurSMahajan/FrontendSIH'
+      'I have designed UI/UX of this app based on the book Atomic Habits, I implemented this app for cross-platform using Flutter and Firebase.',
+    link: 'https://github.com/MayurSMahajan/transformX'
   },
   {
     image:
-      'images/furniture_app.png',
-    title: 
-    'ForMeChair - Furniture App',
-    description: 
-    'I have designed UI/UX for this app. I am currently working on this app. I want to create it using Flutter and Firebase.',
-    link:'https://www.figma.com/file/jVGfvoLgJkoKHlHZ2yywBs/furniture-app?node-id=0%3A1'
+      'images/sih.png',
+    title:
+      'Daemon - Speeches Playing App',
+    description:
+      'This is a cross platform app I created for Smart India Hackathon 2022. This app allows users to watch videos or listen to audios of speeches that are AI generated. The backend is linked with Firebase and is used to store these speeches.',
+    link: 'https://github.com/MayurSMahajan/FrontendSIH'
   },
+
   {
     image:
-    'images/smartlectureapppreview.png',
-    title: 
-    'Smart Lecture App',
+      'images/smartlectureapppreview.png',
+    title:
+      'Smart Lecture App',
     description:
-     'This is a mobile app I created with Java, Android Studio, and Firebase. It allows teachers to create and share notes and assignments. Students can view and download notes and upload assignment answers which are visible to teahers.',
-     link:'https://smartlecture.web.app/'
+      'This is a mobile app I created with Java, Android Studio, and Firebase. It allows teachers to create and share notes and assignments. Students can view and download notes and upload assignment answers which are visible to teahers.',
+    link: 'https://smartlecture.web.app/'
   },
   {
     image:
       'images/valuewealthpreview.png',
-    title: 
-    'Value Wealth Creators Official Website',
-    description: 
-    'I created this website with Express, Node, Mongoose, MongoDB, and React.js. It is a course registration site, where customers can subscribe to a course and pay their fees. '  ,
+    title:
+      'Value Wealth Creators Official Website',
+    description:
+      'I created this website with Express, Node, Mongoose, MongoDB, and React.js. It is a course registration site, where customers can subscribe to a course and pay their fees. ',
     link: 'http://valuewealthcreator.herokuapp.com/'
   },
-  
+
   {
     image:
-    'images/noteskeeper.png',
-    title: 
-    'Notes Keeper React App',
+      'images/noteskeeper.png',
+    title:
+      'Notes Keeper React App',
     description:
-    'An app I created for practicing react. It is a notes keeper app. It allows users to create notes and delete them. I haven not integrated any backend to this app. ',
-    link:'https://github.com/MayurSMahajan/KeeperApp'
+      'An app I created for practicing react. It is a notes keeper app. It allows users to create notes and delete them. I haven not integrated any backend to this app. ',
+    link: 'https://github.com/MayurSMahajan/KeeperApp'
   },
   {
     image:
-    'images/myoldsite.png',
-    title: 
-    'My Old Personal Site',
-    description: 
-    'This is my old personal site. I have created this site using HTML, CSS, and JavaScript.',
-    link:'https://mayursmahajan.github.io/'
+      'images/myoldsite.png',
+    title:
+      'My Old Personal Site',
+    description:
+      'This is my old personal site. I have created this site using HTML, CSS, and JavaScript.',
+    link: 'https://mayursmahajan.github.io/'
   }
 ];
